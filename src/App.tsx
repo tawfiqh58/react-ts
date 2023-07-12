4e2f5c4d20987bb1bfdb0816672a0952505a030d
Tawfiq Hasan <tawfiqhasan58@gmail.com>
2023-07-12 11:03:21 +0600

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomeView from './views/HomeView';
 
 function App() {
-  return <HomeView />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
